7793487ac699f2b3a75362c3d3b37995fb709b08
Gyan Prakash <140909349+GyanPrakashK@users.noreply.github.com>
2024-03-02 21:35:19 +0530

    dhasboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidhWrapper from "@/component/MaxWidhWrapper";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -30,10 +31,21 @@ export default function Home() {
                 clipPath:
                   'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               }}
-              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ea6742] to-[#f759edcb] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
+              className='relative left-[calc(50%-13rem)] aspect-[1155/678]  w-[36.125rem] -translate-x rotate-[29deg] bg-gradient-to-tr from-[#b671e8] via-[#ef7777] to-[#eb54e1cb]  opacity-20 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
             />
-
       </div>
+      <div>
+        <div className=" mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/5 lg:-m-4 lg:rounded-xl lg:p-4">
+                  <Image src="/dashboard-preview.jpg" alt="product preview" height={866} width={1364} quality={100}  />
+                </div>
+
+              </div>
+
+        </div>
+      </div>
+
     </div>
   </div>
   </>
